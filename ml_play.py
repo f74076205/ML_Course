804b3fbf21056b346f2a1eedee6c613575a96f82
f74076205 <62007438+f74076205@users.noreply.github.com>
2020-03-21 11:44:46 +0800

    version1

--- ml_play.py
+++ ml_play.py
@@ -6,7 +6,16 @@ import games.arkanoid.communication as comm
 from games.arkanoid.communication import ( \
     SceneInfo, GameStatus, PlatformAction
 )
-
+def cal_m(x2,y2,m):        
+        #m=(y2-y1)/(x2-x1)
+        x1=-((y2-400)/m-x2)
+        # print(x1)
+        if(x1<0):
+            return -(x1)
+        elif(x1>400):
+            return 400-(x1-400)
+        else:
+            return x1
 def ml_loop():
     """
     The main loop of the machine learning process
@@ -26,7 +35,10 @@ def ml_loop():
     # 2. Inform the game process that ml process is ready before start the loop.
     comm.ml_ready()#一定要保留這行 跟遊戲核心的通道
 
+
     # 3. Start an endless loop.
+    
+    
     while True:
         # 3.1. Receive the scene information sent from the game process.
         scene_info = comm.get_scene_info()#取得遊戲場景資訊 
@@ -43,18 +55,38 @@ def ml_loop():
             continue
 
         # 3.3. Put the code here to handle the scene information
-
+        
         # 3.4. Send the instruction for this frame to the game process
         if not ball_served:
             comm.send_instruction(scene_info.frame, PlatformAction.SERVE_TO_LEFT)
             ball_served = True
+            goal=0
+            m=-1
+            ball_x_pre=95
+            ball_y_pre=400
         else:
-            ball_x=scene_info.ball[0]
-            platform_x=scene_info.platform[0]
             
-            if(platform_x>ball_x):
-               comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
-            elif platform_x<ball_x:
+            ball_x=scene_info.ball[0]
+            ball_y=scene_info.ball[1]
+            platform_x_left=scene_info.platform[0]
+            platform_x_right=scene_info.platform[0]+40
+            #if(ball_x==0 or ball_x==200):
+            m=(ball_y-ball_y_pre)/(ball_x-ball_x_pre)
+            # print(m)
+                
+            
+            goal=cal_m(ball_x,ball_y,m)
+            #print(goal)
+
+            if platform_x_right<goal:
                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
+            elif platform_x_left>goal:
+               comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
             else :
                comm.send_instruction(scene_info.frame, PlatformAction.NONE)
+            
+            
+            ball_x_pre=ball_x
+            ball_y_pre=ball_y
+        
+            
